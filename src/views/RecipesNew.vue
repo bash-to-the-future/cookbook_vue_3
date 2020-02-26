24d86e0463e1290dbabe78ea5c87a9bc8cf1049d
JackMarx <seriousfools@gmail.com>
2020-02-26 11:07:35 -0600

    cat images

--- src/views/RecipesNew.vue
+++ src/views/RecipesNew.vue
@@ -1,40 +1,78 @@
 <template>
   <div class="recipes-new">
     <div class="row">
-      <form class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1" v-on:submit.prevent="createRecipe()">
+      <form class="col-sm-6" v-on:submit.prevent="createRecipe()">
         <h1 class="text-center mb-5">New Recipe</h1>
-
-        <ul>
-          <li class="text-danger" v-for="error in errors">{{ error }}</li>
-        </ul>
 
         <div class="form-group">
           <label>Title: </label>
-          <input class="form-control" type="text" v-model="title">
+          <input class="form-control" type="text" :class="{'is-invalid': errors.title }" v-model="title">
+          <div class="invalid-feedback">
+            <ul>
+              <li class="text-danger" v-for="error in errors.title">{{ error }}</li>
+            </ul>
+          </div>
         </div>
 
         <div class="form-group">
           <label>Prep Time: </label>
-          <input class="form-control" type="number" v-model="prepTime">
+          <input class="form-control" type="text" :class="{'is-invalid': errors.prep_time }" v-model="prepTime">
+          <div class="invalid-feedback">
+            <ul>
+              <li class="text-danger" v-for="error in errors.prep_time">{{ error }}</li>
+            </ul>
+          </div>
         </div>
 
         <div class="form-group">
           <label>Ingredients: </label>
-          <input class="form-control" type="text" v-model="ingredients">
+          <input class="form-control" type="text" :class="{'is-invalid': errors.ingredients }" v-model="ingredients">
+          <div class="invalid-feedback">
+            <ul>
+              <li class="text-danger" v-for="error in errors.ingredients">{{ error }}</li>
+            </ul>
+          </div>
         </div>
 
         <div class="form-group">
           <label>Directions: </label>
-          <input class="form-control" type="text" v-model="directions">
+          <input 
+            class="form-control" 
+            type="text" 
+            :class="{'is-invalid': errors.directions, 'is-valid': Object.keys(errors).length !== 0 && !errors.directions }" 
+            v-model="directions"
+            >
+          <div class="invalid-feedback">
+            <ul>
+              <li class="text-danger" v-for="error in errors.directions">{{ error }}</li>
+            </ul>
+          </div>
+          <div class="valid-feedback">
+            <ul>
+              <li class="text-success" v-for="error in errors.directions">{{ error }}</li>
+            </ul>
+          </div>
         </div>
 
         <div class="form-group">
           <label>Image Url: </label>
-          <input class="form-control" type="text" v-model="imageUrl">
+          <input class="form-control" type="text" :class="{'is-invalid': errors.image_url }" v-model="imageUrl">
+          <div class="invalid-feedback">
+            <ul>
+              <li class="text-danger" v-for="error in errors.image_url">{{ error }}</li>
+            </ul>
+          </div>
         </div>
 
         <input class="btn btn-info" type="submit" value="Create">
       </form>
+
+      <div v-if="status" class="col-sm-6">
+        <img class="img-fluid mt-5" :src="`https://http.cat/${status}`">
+      </div>
+      <div v-else class="col-sm-6">
+        <img class="img-fluid mt-5" src="https://en.bcdn.biz/Images/2018/6/12/27565ee3-ffc0-4a4d-af63-ce8731b65f26.jpg">
+      </div>
     </div>
   </div>
 </template>
@@ -52,7 +90,8 @@ export default {
       ingredients: "",
       directions: "",
       imageUrl: "",
-      errors: []
+      errors: {},
+      status: ""
     };
   },
   created: function() {},
@@ -69,9 +108,13 @@ export default {
       axios
         .post("/api/recipes", clientParams)
         .then(response => {
-          this.$router.push("/recipes");
+          this.$router.push("/recipes/" + response.data.id);
         }).catch(error => {
-          this.errors = error.response.data.errors;
+          if (error.response.data.errors) {
+            this.errors = error.response.data.errors;
+          }
+
+          this.status = error.response.status;
         });
     }
   }
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -8,7 +8,13 @@
         </ul>
         <div class="form-group">
           <label>Name:</label> 
-          <input type="text" class="form-control" v-model="name">
+          <input type="text" class="form-control" v-bind:class="{'is-valid': name === 'bob'}" v-model="name">
+          <div class="valid-feedback">
+            Hey, Your name is bob too!
+          </div>
+          <div class="invalid-feedback">
+            You did it wrong
+          </div>
         </div>
         <div class="form-group">
           <label>Email:</label>
@@ -20,7 +26,20 @@
         </div>
         <div class="form-group">
           <label>Password confirmation:</label>
-          <input type="password" class="form-control" v-model="passwordConfirmation">
+          <input 
+            type="password" class="form-control" 
+            :class="{
+                     'is-invalid': passwordConfirmation !== '' && password !== passwordConfirmation,
+                     'is-valid': passwordConfirmation !== '' && password === passwordConfirmation
+                     }" 
+            v-model="passwordConfirmation"
+          >
+          <div class="valid-feedback">
+            You password and password confirmation match.
+          </div>
+          <div class="invalid-feedback">
+            Your password must match your password confirmation.
+          </div>
         </div>
         <input type="submit" class="btn btn-primary" value="Submit">
       </form>      
